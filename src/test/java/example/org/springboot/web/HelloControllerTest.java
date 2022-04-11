d9b7630c7a5a53b575fde843f138f807dc3f7bcc
somin <83453597+yygs321@users.noreply.github.com>
2022-04-11 15:03:53 +0900

    index.html ㅇㅔ서 <p> 삭제

--- src/test/java/example/org/springboot/web/HelloControllerTest.java
+++ src/test/java/example/org/springboot/web/HelloControllerTest.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-
 @RunWith(SpringRunner.class)//테스트 진행 시 JUnit에 내장된 실행자 외에 다른 실행자 실행
 //여기서는 SpringRunner라는 스프링 실행자 사용
 //스프링부트 테스트와 JUnit 사이에 연결자 역할
